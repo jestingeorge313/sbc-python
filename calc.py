86286a540c3b8627e75e629e28cdc27e958a4fca
jestingeorge313 <jestingeorge313@gmail.com>
2019-02-23 12:52:54 +0530

    first commit

---
+++ calc.py
@@ -0,0 +1,9 @@
+import add
+
+# num1=int(input("Enter the first number"))
+# num2=int(input("Enter the second number"))
+# result=add.addition(num1,num2)
+# print(result)
+
+print(__name__)
+add.display()
